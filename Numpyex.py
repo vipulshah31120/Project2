4dffa0342e0c889bbf30779a55b63321c3fca408
vipulshah31120 <75607031+vipulshah31120@users.noreply.github.com>
2021-05-05 14:32:47 +0530

    NumPy

---
+++ Numpyex.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+matrix = np.array([[1,2,3,], [4,5,6], [7,8,9]])
+print(matrix)
+print('Type of Array: ', type(matrix))
+print('No. of Dimensions: ', matrix.ndim)
+#print('Shape of Array: '. matrix.shape)
+print('Size of Array: ', matrix.size)
+print('Array stores element of types: ', matrix.dtype)
